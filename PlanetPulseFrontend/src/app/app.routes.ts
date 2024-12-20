1c7a7f02d199a120719a423c0252d12ffaac9f5d
ettjob15 <ettjob15@htlkaindorf.at>
2024-12-20 11:45:44 +0100

    Added routes to components

--- PlanetPulseFrontend/src/app/app.routes.ts
+++ PlanetPulseFrontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PolutionMapComponent } from './polution-map/polution-map.component';
+import { Co2CalculatorComponent } from './co2-calculator/co2-calculator.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/polution-map', pathMatch: 'full' },
+  { path: 'polution-map', component: PolutionMapComponent  },
+  { path: 'calculator', component: Co2CalculatorComponent},
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'userprofile', component: UserProfileComponent},
+];
