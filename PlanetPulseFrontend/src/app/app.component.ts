3d8918aac86b3a394e07c94b37a81bacf09a5b9f
ettjob15 <ettjob15@htlkaindorf.at>
2024-12-20 11:20:41 +0100

    Changed Title Page

--- PlanetPulseFrontend/src/app/app.component.ts
+++ PlanetPulseFrontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ 
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    AsyncPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
