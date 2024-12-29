4a8122b351e61dc2905e7b941677acdcce67f45f
ettjob15 <ettjob15@htlkaindorf.at>
2024-12-29 10:50:35 +0100

    Added Air quality table

--- PlanetPulseFrontend/src/app/services/openweather.service.ts
+++ PlanetPulseFrontend/src/app/services/openweather.service.ts
@@ -10,12 +10,12 @@ private API_KEY = '96cf371a37e6bdeaa0cf099db6237485';
 
 constructor(private http: HttpClient) {}
   getCityCoordinates(cityName: string): Observable<any> {
-    const url = 'https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${this.API_KEY}';
+    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${this.API_KEY}`;
     return this.http.get(url);
   }
 
   getAirPollution(lat: number, lon: number): Observable<any> {
-    const url = 'https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${this.API_KEY}';
+    const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${this.API_KEY}`;
     return this.http.get(url);
   }
 }
